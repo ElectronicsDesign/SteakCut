3fe24f8230acfd3acdf60c76a6fcff5c5a7968de
Sakutyobi <45097311+Sakutyobi@users.noreply.github.com>
2018-11-19 14:21:22 +0900

    クリック時に音が大きくなるように変更

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,7 @@
 char Key[256];
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	if (ChangeWindowMode(FALSE) != DX_CHANGESCREEN_OK || DxLib_Init() == -1) return -1; //初期化処理
+	if (ChangeWindowMode(TRUE) != DX_CHANGESCREEN_OK || DxLib_Init() == -1) return -1; //初期化処理
 
 	int image = LoadGraph("./img/Knife_a.png");
 	int imgBack = LoadGraph("./img/Main.png");
@@ -20,22 +20,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;    // エラーが起きたら直ちに終了
 	}
 
-	int Mouse = GetMouseInput();
+	//int Mouse = GetMouseInput();
 	int Handle = LoadSoundMem("./snd/Start.mp3");
+	int Handle1 = LoadSoundMem("./snd/Center.mp3");
+
+
 
 	SetMouseDispFlag(TRUE);
 	StringCr = GetColor(255, 255, 255);
 
 	BoxCr = GetColor(0, 0, 0);
 
-	ChangeVolumeSoundMem(255 * 20, Handle);
-	PlaySoundMem(Handle, DX_PLAYTYPE_BACK); // 効果音を再生する
+	ChangeVolumeSoundMem(255 * 30/100, Handle);
+	ChangeVolumeSoundMem(255 * 50/ 100, Handle1);
 
 
 	while (!ProcessMessage() && !ClearDrawScreen() && !GetHitKeyStateAll(Key) && !Key[KEY_INPUT_ESCAPE]) {
 		//↑メッセージ処理 ↑画面をクリア ↑キーボード入力状態取得 ↑ESCが押されていない
 
 		int x, y;
+		int Mouse = GetMouseInput();
 		GetMousePoint(&x, &y);
 		GetMousePoint(&MouseX, &MouseY);
 
@@ -59,6 +63,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		DrawFormatString(0, 20, 0xffffff, "%d", image);
 		DrawFormatString(0, 40, 0xffffff, "%d", imgBack);
 		DrawFormatString(0, 60, 0xffffff, "%d", Handle);
+
+
+		if ((Mouse & MOUSE_INPUT_LEFT)!=0){
+			PlaySoundMem(Handle1, DX_PLAYTYPE_BACK, FALSE); // 効果音を再生する
+		}
+		else {
+			PlaySoundMem(Handle, DX_PLAYTYPE_BACK, FALSE); // 効果音を再生する
+		}
+
+		
 
 
 	
