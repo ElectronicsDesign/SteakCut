ef9713fcdbb75a414ebe483722c1a9fb0d21fc58
Sakutyobi <45097311+Sakutyobi@users.noreply.github.com>
2018-11-19 14:43:25 +0900

    肉　三枚に割れる

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int image = LoadGraph("./img/Knife_a.png");
 	int imgBack = LoadGraph("./img/Main.png");
 	int imgmiddle = LoadGraph("./img/meet_main.png");
+	int imgmiddle3 = LoadGraph("./img/meet_g2.png");
+	int imgmiddle4 = LoadGraph("./img/meet_g3.png");
+	int imgmiddle5 = LoadGraph("./img/meet_g1.png");
 
 	char StrBuf[128], StrBuf2[32];
 	int MouseX, MouseY;
@@ -31,8 +34,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	BoxCr = GetColor(0, 0, 0);
 
-	ChangeVolumeSoundMem(255 * 30/100, Handle);
-	ChangeVolumeSoundMem(255 * 50/ 100, Handle1);
+	ChangeVolumeSoundMem(0 * 30/100, Handle);
+	ChangeVolumeSoundMem(0 * 50/ 100, Handle1);
 
 
 	while (!ProcessMessage() && !ClearDrawScreen() && !GetHitKeyStateAll(Key) && !Key[KEY_INPUT_ESCAPE]) {
@@ -44,7 +47,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		GetMousePoint(&MouseX, &MouseY);
 
 		DrawRotaGraph(300, 250, 0.5, 0.0, imgBack, TRUE);
-		DrawRotaGraph(300, 200, 0.5, 0.0, imgmiddle, TRUE);
+
+		if ((Mouse & MOUSE_INPUT_LEFT) != 0) {
+			DrawRotaGraph(450, 200, 0.2, 0.0, imgmiddle3, TRUE);
+			DrawRotaGraph(350, 200, 0.2, 0.0, imgmiddle4, TRUE);
+			DrawRotaGraph(145, 200, 0.2, 0.0, imgmiddle5, TRUE);
+		}
+		else {
+			DrawRotaGraph(300, 200, 0.5, 0.0, imgmiddle, TRUE);
+		}
 		DrawRotaGraph(x, y, 0.5, 0.0, image, TRUE);
 
 
